a53011bfc293fdef595c0f418cf1fa4dcea09162
oleksiikoval <lexi4.koval@gmail.com>
2023-03-16 09:01:42 -0400

    Designing database

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.cydeo.controller;
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.impl.RoleServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserController {
 
 
+    RoleServiceImpl roleService;
     @GetMapping("/create")
     public String userCreate(Model model){
 
         model.addAttribute("user", new UserDTO());
+        //                                           //find roles from DB
+        model.addAttribute("listRoles", roleService.findAll());
         return "/user/create";
 
     }
---
+++ src/main/java/com/cydeo/service/CrudService.java
@@ -0,0 +1,10 @@
+package com.cydeo.service;
+
+import java.util.List;
+
+public interface CrudService <T, ID>{
+    T save(T user);
+    T findById(ID username);
+    List<T> findAll();
+    void deleteById(ID username);
+}
